472d4b2e1747c49ad54f7567892196c9f5aa9b33
Leonardo <leo.braga@eaportal.org>
2025-04-25 11:40:50 -0300

    Exemplo 2 , feito

--- exemplo1 main.js
+++ exemplo1 main.js
@@ -1,3 +1,5 @@
+// Exemplo 1
+
 function mensagem() {
     alert("Seja bem-vindo(a)!")
     console.log("Seja bem-vindo(a)!")
@@ -11,3 +13,17 @@ mensagem()
 mensagem()
 mensagem()
 mensagem()
+
+// Exemplo 2
+
+function somar(numero1, numero2) {
+    console.log(numero1 + numero2)
+
+}
+
+let numero1 = 20
+let numero2 = 30
+
+somar(numero1, numero2)
+somar(34, 50)
+somar(34, 200)
