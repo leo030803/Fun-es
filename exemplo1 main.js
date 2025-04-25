54f5d73bd3ab16687b68a570388c0a1181b943d3
Leonardo <leo.braga@eaportal.org>
2025-04-25 11:55:42 -0300

    Exemplo 3 , feito

--- exemplo1 main.js
+++ exemplo1 main.js
@@ -14,6 +14,7 @@ mensagem()
 mensagem()
 mensagem()
 
+
 // Exemplo 2
 
 function somar(numero1, numero2) {
@@ -27,3 +28,31 @@ let numero2 = 30
 somar(numero1, numero2)
 somar(34, 50)
 somar(34, 200)
+
+
+// Exemplo 3
+
+function raizQuadrada(numero) {
+
+    console.log(Math.sqrt(numero))
+
+}
+
+function potenciacao(valor1, valor2) {
+    console.log(Math.pow(valor1, valor2))
+
+}
+
+function multiplicacao(valor1, valor2) {
+      console.log(valor1 * valor2)
+
+}
+
+
+let numero1 = Number(prompt("Digite o numero 1:"))
+let numero2 = Number(prompt("Digite o numero 2:"))
+
+raizQuadrada(numero1)
+potenciacao(numero1, numero2)
+multiplicacao(numero2, numero1)
+
